cfb07050dc366d19861325651e63e9dc51ad9048
Dejan Tomic <dejan.tomic@smava.de>
2019-04-16 00:40:27 +0200

    Add crossorigin proxy

--- src/services/urls.js
+++ src/services/urls.js
@@ -1,2 +1,2 @@
 export const itunesTopAlbums = 'https://itunes.apple.com/us/rss/topalbums/limit=100/json';
-export const itunesAlbumDetails = 'https://itunes.apple.com/search?term=?';
+export const itunesAlbumDetails = 'https://cors-anywhere.herokuapp.com/https://itunes.apple.com/search?term=?';
